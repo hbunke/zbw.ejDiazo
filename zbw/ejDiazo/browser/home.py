eff285c8d19e8a0844fb88436bbd0178cee3d2b8
Hendrik Bunke <h.bunke@zbw.eu>
2016-06-30 14:20:45 +0200

    first draft frontpage

---
+++ zbw/ejDiazo/browser/home.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+
+# Dr. Hendrik Bunke <h.bunke@zbw.eu>
+# German National Library of Economics (ZBW)
+# http://zbw.eu/
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+#from zope.interface import Interface
+#from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
+from Products.AdvancedQuery import In, Eq, And, Or
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from Products.eJournal.configpanel import IEjournalConf
+
+
+class View(BrowserView):
+    """
+    """
+
+    template = ViewPageTemplateFile("home.pt")
+
+    def __call__(self):
+        return self.template()
+
+    def get_latest_ja(self):
+        catalog=getToolByName(self.context, "portal_catalog")
+        brains = catalog(portal_type='JournalPaper', review_state="published",
+                sort_on="created", sort_order="descending", sort_limit=1)[:1]
+        return [brain.getObject() for brain in brains]
+
+
+    def get_latest_dp(self):
+        catalog=getToolByName(self.context, "portal_catalog")
+        brains = catalog(portal_type='DiscussionPaper', review_state=("discussible", "rejected"),
+                sort_on="created", sort_order="descending", sort_limit=1)[:1]
+        return [brain.getObject() for brain in brains]
