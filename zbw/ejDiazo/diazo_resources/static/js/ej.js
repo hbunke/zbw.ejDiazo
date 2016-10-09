c142685753df77d2c0bc4a8718acc48045f9aed6
Hendrik Bunke <h.bunke@zbw.eu>
2016-10-09 16:19:59 +0200

    new Logo

--- zbw/ejDiazo/diazo_resources/static/js/ej.js
+++ zbw/ejDiazo/diazo_resources/static/js/ej.js
@@ -5,11 +5,17 @@
 var showjumbo = document.getElementById('showjumbo');
 var hidejumbo = document.getElementById('hidejumbo');
 var jumbotron = document.getElementById('ejumbo');
+var loggo = document.getElementById('loggo');
 $(jumbotron).show();
 $(showjumbo).hide();
 
+if (jumbotron) {
+    $(loggo).hide()
+}
+
 $(showjumbo).on('click', function() {
     show_jumbo();
+
 });
 
 $(hidejumbo).on('click', function() {
@@ -20,10 +26,12 @@ function show_jumbo() {
     $(jumbotron).show();
     $(showjumbo).hide();
     $(hidejumbo).show();
+    $(loggo).hide();
 }
 
 function hide_jumbo() {
     $(jumbotron).hide();
     $(showjumbo).show();
     $(hidejumbo).hide();
+    $(loggo).show();
 }
