b70ed2048ef947accb8f793bbdaf6df9ddd26cc7
Hendrik Bunke <h.bunke@zbw.eu>
2016-12-09 11:43:43 +0100

    finalize frontpage

--- zbw/ejDiazo/browser/home.py
+++ zbw/ejDiazo/browser/home.py
@@ -20,14 +20,14 @@ class View(BrowserView):
     def get_latest_ja(self):
         catalog = getToolByName(self.context, "portal_catalog")
         brains = catalog(portal_type='JournalPaper', review_state="published",
-                         sort_on="created", sort_order="descending", sort_limit=1)[:1]
+                         sort_on="created", sort_order="descending", sort_limit=3)[:3]
         return [brain.getObject() for brain in brains]
 
     def get_latest_dp(self):
         catalog = getToolByName(self.context, "portal_catalog")
         brains = catalog(portal_type='DiscussionPaper',
                          review_state=("discussible", "rejected"),
-                         sort_on="created", sort_order="descending", sort_limit=1)[:1]
+                         sort_on="created", sort_order="descending", sort_limit=3)[:3]
         return [brain.getObject() for brain in brains]
 
     def get_teaser(self):
@@ -44,3 +44,11 @@ class View(BrowserView):
                          sort_order='reverse',
                          review_state='published')[:3]
         return brains
+
+    def get_recent_comments(self):
+        """returns recent comments sorted by date
+        """
+        catalog = getToolByName(self.context, "portal_catalog")
+        brains = catalog(portal_type="Comment", review_state="published",
+                         sort_on="Date", sort_order="reverse")[:5]
+        return [brain.getObject() for brain in brains]
